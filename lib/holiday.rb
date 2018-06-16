2065059576fd78728490718f0214d683834159a9
Kevin Chi <kevinchi118@gmail.com>
2018-06-16 00:57:39 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -26,12 +26,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-    holiday_hash.each do |seasons|
-        if seasons == :season
-            seasons = {holiday_name => supply_array}
-        end
-    end
-    holiday_hash
+    holiday_hash[season] = {holiday_name => supply_array}
 end
 
 def all_winter_holiday_supplies(holiday_hash)
