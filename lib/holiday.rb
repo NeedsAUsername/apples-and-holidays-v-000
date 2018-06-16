813ff085bba45831d2ac47708648dd7249fbed2d
Kevin Chi <kevinchi118@gmail.com>
2018-06-16 01:49:42 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-    holiday_hash.each do |season, season_info|
+    holiday_hash.collect do |season, season_info|
         season_info.collect do |holiday, supplies|
             supplies.include?("BBQ") ? holiday : nil
         end
